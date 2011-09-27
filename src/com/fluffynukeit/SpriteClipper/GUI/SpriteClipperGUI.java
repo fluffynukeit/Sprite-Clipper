87c37f816f1e2c2c93e2b52b634064d3e5840970
Daniel Austin <dan@fluffynukeit.com>
2011-09-26 22:23:11 -0400

    Fixed a GUI component that was slightly mishapen after adding center anchor point.

--- src/com/fluffynukeit/SpriteClipper/GUI/SpriteClipperGUI.java
+++ src/com/fluffynukeit/SpriteClipper/GUI/SpriteClipperGUI.java
@@ -472,10 +472,10 @@ public class SpriteClipperGUI extends javax.swing.JFrame implements Observer,
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(mergeButton, javax.swing.GroupLayout.DEFAULT_SIZE, 74, Short.MAX_VALUE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(storeClipsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(24, 24, 24))
         );
 
         jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {mergeButton, reshapeButton, selectAllButton, selectNoneButton});
